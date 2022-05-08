0789d7f1aa036ef477700ab71edb1b287083f4e8
Alexander Skotnikov <92207202+merlich21@users.noreply.github.com>
2022-05-08 20:17:47 +0300

    added +1 in malloc for '\0'

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -20,7 +20,7 @@ char	*ft_strjoin(char const	*s1, char const	*s2)
 	i = 0;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	new_str = malloc(ft_strlen(s1) + ft_strlen(s2));
+	new_str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (NULL == new_str)
 		return (NULL);
 	else
